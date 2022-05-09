298a58deefa06b72e7807f010c07054556f7b212
ParkHeeMin <gmlals9628@naver.com>
2022-05-09 11:19:29 +0900

    Update P_단체사진찍기.java

--- Week10/P_단체사진찍기.java
+++ Week10/P_단체사진찍기.java
@@ -1,41 +1,67 @@
+import java.util.*;
 class Solution {
-     public boolean[] used;
-    public char[] arr;
-    public String[] data;
-    public int answer;
-    public int solution(int n, String[] data) {
-        this.answer = 0;
-        this.data = data;
-        arr = new char[]{'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'}; 
-        used = new boolean[arr.length];        
-        dfs("", 0);
+    static HashMap<Character, Integer> friend;
+    static int list[], answer;
+    static boolean isVisited[];
+    static String[] s_data;
+ 
+    static int solution(int n, String[] data) {
+        s_data = data;
+ 
+        friend = new HashMap<Character, Integer>();
+        friend.put('A', 0);
+        friend.put('C', 1);
+        friend.put('F', 2);
+        friend.put('J', 3);
+        friend.put('M', 4);
+        friend.put('N', 5);
+        friend.put('R', 6);
+        friend.put('T', 7);
+ 
+        answer = 0;
+        list = new int[8]; // 알파벳이 들어갈 위치
+        isVisited = new boolean[8];
+        perm(0);
+ 
         return answer;
     }
-    
-    public void dfs(String str, int len){
-        if(len == arr.length) {
-            if(isPossible(str)) answer++;
+ 
+    private static void perm(int cnt) {
+        if (cnt == 8) {
+            if (isCorrect())
+                answer++;
             return;
         }
-        for(int i = 0; i < arr.length; i++){
-            if(used[i]) continue;
-            used[i] = true;
-            dfs(str+arr[i], len+1);
-            used[i] = false;
+ 
+        for (int i = 0; i < 8; i++) {
+            if (isVisited[i])
+                continue;
+ 
+            isVisited[i] = true;
+            list[cnt] = i;
+            perm(cnt + 1);
+            isVisited[i] = false;
         }
     }
-    
-    public boolean isPossible(String str){
-        for(String rule : data){
-            int idx1 = str.indexOf(String.valueOf(rule.charAt(0)));
-            int idx2 = str.indexOf(String.valueOf(rule.charAt(2)));
-            char oper = rule.charAt(3);
-            int diff = Integer.parseInt(String.valueOf(rule.charAt(4)));
-            int realDiff = Math.abs(idx1-idx2)-1;
-            if ((oper == '<') && !(realDiff < diff)) return false;
-            else if ((oper == '>') && !(realDiff > diff)) return false;
-            else if ((oper == '=') && !(realDiff == diff)) return false;
+ 
+    private static boolean isCorrect() {
+        for (String s : s_data) {
+            int friend1 = list[friend.get(s.charAt(0))];
+            int friend2 = list[friend.get(s.charAt(2))];
+            char op = s.charAt(3);
+            int d = s.charAt(4) - '0' + 1;
+ 
+            int distance = Math.abs(friend1 - friend2);
+            if (op == '=') {
+                if (distance != d)
+                    return false;
+            } else if (op == '>' ) {
+                if(distance <= d)
+                    return false;
+            } else if (distance >= d)
+                return false;
         }
+ 
         return true;
     }
 }
